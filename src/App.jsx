9aa42919c212c78b7b431743f5b08ef52f04273a
Juan Hernan <128071729+JuanHernan123@users.noreply.github.com>
2023-06-06 17:43:51 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+function App(){
+  return (
+   <div className="container text-center">
+     <div className="row">
+       <div className="col">
+     <h1>Shopping List</h1>
+     <hr />
+     </div>
+     </div>
+     <div className="row">
+       <div className="col-1">
+         <input type="checkbox" />
+     </div>
+     <div className="col text-start">
+     1 kg
+     </div>
+       <div className="col-5 col-md-7 text-start ">
+         Tortillas
+     </div>
+     <div className="col-4 col-md-3 btn-group btn-group-sm">
+       <button className="btn btn-outline-primary">
+       < i className="bi bi-pencil-square"></i>
+       </button>
+       <button className="btn btn-outline-danger">
+         <i className="bi bi-trash2-fill"></i>
+       </button>
+     </div>
+   </div>
+
+   <div className="row">
+       <div className="col-1">
+         <input type="checkbox" />
+     </div>
+     <div className="col text-start">
+     1 lt
+     </div>
+       <div className="col-5 col-md-7 text-start">
+         Aceite
+     </div>
+     <div className="col-4 col-md-3 btn-group btn-group-sm text-end">
+       <button className="btn btn-outline-primary">
+       < i className="bi bi-pencil-square"></i>
+       </button>
+       <button className="btn btn-outline-danger">
+         <i className="bi bi-trash2-fill"></i>
+       </button>
+     </div>
+   </div>
+
+
+ </div> 
+  )
+}
+
+export default App
